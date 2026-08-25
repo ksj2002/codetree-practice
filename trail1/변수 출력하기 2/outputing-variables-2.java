df1916cb0d67b22d7b8e596ad107491fd86ac20e
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-08-25 11:54:47 +0900

    [Passed] 변수 출력하기 2 (89ms, 9MB)

---
+++ trail1/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char b = 'C';
+
+        System.out.println(b + "!.....!" + a);
+
+
+    }
+}
