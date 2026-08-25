faa47720f23d033edd612e3a1578c7bf30e73b97
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-08-25 11:40:28 +0900

    [Passed] 정수 선언하고 뺄셈 (99ms, 9MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 13;
+
+        System.out.println(a + " - " + b + " = " + (a-b));
+
+    }
+}
