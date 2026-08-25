ef133a15a0d4b29924a81240e2205cdebca0312c
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-08-25 11:42:33 +0900

    [Passed] 정수 선언하고 곱 출력 (95ms, 9MB)

---
+++ trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 26;
+        int b = 5;
+
+        System.out.println(a + " * " + b + " = " + (a * b));
+        
+
+    }
+}
