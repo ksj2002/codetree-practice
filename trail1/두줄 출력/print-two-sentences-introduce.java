56e6b35d3f41b8b2f4301a4b029e8f9691bb3b1f
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-08-25 11:37:43 +0900

    [Passed] 두줄 출력 (94ms, 9MB)

---
+++ trail1/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!");
+
+    }
+}
