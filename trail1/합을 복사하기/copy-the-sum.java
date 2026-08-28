faaf8329ed6d576ee5c0a0c9b421cab268454efe
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-08-29 01:46:27 +0900

    [Passed] 합을 복사하기 (89ms, 9MB)

---
+++ trail1/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int sum = a + b + c;
+        
+        a = b = c = sum;
+        System.out.println(a + " " + b + " " + c);
+
+    }
+}
