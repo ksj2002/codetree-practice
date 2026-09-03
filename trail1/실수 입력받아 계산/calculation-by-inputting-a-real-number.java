6dff117b9b8eb5ced80cb8d8a8448e386679c966
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-09-03 13:14:31 +0900

    [Passed] 실수 입력받아 계산 (162ms, 12MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        
+        double sum = a+ b;
+        
+        
+
+        System.out.printf("%.2f", sum);
+
+
+    }
+}
