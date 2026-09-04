23470d2577d24909f596329797596c529b284816
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-09-04 22:35:08 +0900

    [Passed] 1시간 뒤 시간 출력 (126ms, 10MB)

---
+++ trail1/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String time = sc.next();
+
+        String[] arr = time.split(":");
+        
+        System.out.println(Integer.parseInt(arr[0]) + 1 + ":" + arr[1]);
+    }
+}
