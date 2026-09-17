470a79c87cd3c04870dbebb37fa6d7c689c23c87
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-09-17 19:56:14 +0900

    [Passed] 감소시키며 출력하기 2 (91ms, 9MB)

---
+++ trail1/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {    
+        int i = 26;
+        while(i >= 10){
+
+            System.out.print(i + " ");
+
+            i--;
+        }
+    }
+}
+
