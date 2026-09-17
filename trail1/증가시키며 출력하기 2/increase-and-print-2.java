fb91ca7847c739c530c1737880c26eca6d413929
ksj2002 <162393764+ksj2002@users.noreply.github.com>
2026-09-17 19:43:31 +0900

    [Passed] 증가시키며 출력하기 2 (93ms, 9MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int i = 10;
+        
+        while(i <= 26){
+            System.out.print(i + " ");
+            
+            i++;
+        }
+    }
+}
+
